150533717bdb4f5ce9c0ad95c9fdf4f3100749a6
arkadyjczyk91 <arkadiuszprzychocki@gmail.com>
2025-05-22 16:13:23 +0200

    remove frontend static file handling from server; clean up PWA support

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
-import path from 'path';
 import connectDB from './config/db';
 import authRoutes from './routes/auth';
 import transactionRoutes from './routes/transaction';
@@ -23,23 +22,10 @@ app.use(cors());
 // Definicja routerów
 app.use('/api/auth', authRoutes);
 app.use('/api/transactions', transactionRoutes);
-app.use("/api/category-budget", categoryBudgetRoutes);
+app.use('/api/category-budget', categoryBudgetRoutes);
 app.use('/api/saving-goals', savingGoalRoutes);
-// Obsługa PWA - statyczne pliki dla frontendu
-if (process.env.NODE_ENV === 'production') {
-    // Statyczne pliki
-    app.use(express.static('client/build'));
 
-    // Service Worker
-    app.get('/service-worker.js', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '../client/build/service-worker.js'));
-    });
-
-    // Wszystkie pozostałe ścieżki kierowane do aplikacji React
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '../client/build/index.html'));
-    });
-}
+// Usunięto obsługę statycznych plików frontendu
 
 const PORT = process.env.PORT || 5000;
 
