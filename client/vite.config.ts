54a38909fbeefb69df3a2d2ba7a25ba2a4e0e402
arkadyjczyk91 <arkadiuszprzychocki@gmail.com>
2025-05-22 16:20:32 +0200

    update package.json for deployment; add homepage and deploy scripts using gh-pages

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,13 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000', // Adres serwera backendowego
-        changeOrigin: true,
-        secure: false
-      }
-    }
-  }
+  base: '/pbm/',
 });
